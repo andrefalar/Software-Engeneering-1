15f797344501b79c666a92de4ec62b7de9e28240
andrefalar <andre.alarcon@proton.me>
2025-07-11 15:47:37 -0500

    Eliminados archivos de base de datos y actualizadas dependencias de PyQt5 en requirements.txt

--- Proyecto/frontend/test_gui.py
+++ Proyecto/frontend/test_gui.py
@@ -1,6 +1,7 @@
+# test_gui.py
 from PyQt5.QtWidgets import QApplication, QLabel
 
 app = QApplication([])
-label = QLabel('¡Hola, FortiFile!')
+label = QLabel("¡Hola desde WSLg con PyQt5!")
 label.show()
 app.exec_()
